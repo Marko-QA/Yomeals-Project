62b127e80b279c86477698c49775c9d2cb2440e4
Marko Jovanovic <marko.jovanovic.qa@gmail.com>
2021-10-17 15:16:34 +0200

    Basic Page updated

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -1,18 +1,14 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasicPage {
-	
-	private WebDriver driver;
-	private WebDriverWait wait;
-	
-	public BasicPage(WebDriver driver, WebDriverWait wait) {
-	
-		this.driver = driver;
-		this.wait = wait;
-	}
-	
-	
+
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected JavascriptExecutor js;
+	protected Actions actions;
 }
